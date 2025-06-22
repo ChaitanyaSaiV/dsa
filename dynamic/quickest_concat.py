bde91466dd7ac5b11d883d290af9db08f8f98df1
Cveeravalli-desktop <cveeravalli@isc2.org>
2025-06-22 13:27:10 -0500

    added dynamic dir

---
+++ dynamic/quickest_concat.py
@@ -0,0 +1,12 @@
+""" quickest concat
+Write a function, quickest_concat, that takes in a string and a list of words as arguments. The function should return the minimum number of words needed to build the string by concatenating words of the list. If it is not possible to build the string, then return -1.
+
+You may use words of the list as many times as needed. """
+
+
+def quickest_concat(s, words):
+  pass # todo
+
+
+
+quickest_concat('caution', ['ca', 'ion', 'caut', 'ut']) # -> 2
